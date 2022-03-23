d0d2503e26fa38b4a80da42f4516aa5bbecaab65
User <User@DESKTOP-802SV85>
2022-03-23 19:12:24 +0200

    akt4

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -1,3 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int summa(int a, int b) {
+
+    if (b == a - 1) return 0;
+    return b + summa(a, b - 1);
+}
 
 int main()
+{
+    setlocale(0, "");
+    int a, b;
+    cout << "¬ведите 1-ое число: ";
+    cin >> a;
+    cout << "¬ведите 2-ое число: ";
+    cin >> b;
+
+    cout << summa(a, b);
+
+    return 0;
+}
